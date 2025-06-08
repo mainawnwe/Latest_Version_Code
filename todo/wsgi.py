8839c756831041c9d634a0c6783624b78f0901dc
KO NAING KYAW <mycontact.com101@gmail.com>
2025-06-08 17:03:38 +0630

    Update wsgi.py

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Replace with your actual path
+project_path = '/home/ackermancode/Latest_Version_code'
+if project_path not in sys.path:
+    sys.path.append(project_path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'toto.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
-
 application = get_wsgi_application()
+
